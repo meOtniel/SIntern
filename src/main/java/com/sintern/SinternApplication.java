8a3631d5835d0d5894c97cffefe9eac8c9b9ccfc
DascaluCosmin <dascalu.ac@gmail.com>
2021-10-24 17:51:41 +0300

    Initializing the structure of the project

---
+++ src/main/java/com/sintern/SinternApplication.java
@@ -0,0 +1,13 @@
+package com.sintern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SinternApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sintern/SinternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sintern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
