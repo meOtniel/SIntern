9cf33ebee8924fb815d1296172b08a2388807223
Daniel Burtic <patricia.burtic@gmail.com>
2021-11-13 13:26:12 +0200

    AuthenticationFilter changes

--- src/main/java/com/sintern/security/filters/AuthenticationFilter.java
+++ src/main/java/com/sintern/security/filters/AuthenticationFilter.java
@@ -59,8 +59,6 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
             }
         }
 
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-
         return authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
                 creds.getEmail(), creds.getPassword(), new ArrayList<>()));
     }
